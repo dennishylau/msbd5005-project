6147248c321f544916cc32ec0ac5c80fd63271a1
Dennis Lau <dennishylau@gmail.com>
2022-04-15 14:26:36 +0800

    feat: refactor data load into separate files, preproc imd dot data

---
+++ preprocess/imf_dot.py
@@ -0,0 +1,44 @@
+# %%
+import pandas as pd
+import re
+
+
+def preprecess_imf_dot():
+
+    # download this file separately, too large for git
+    df_imf_dot_raw = pd.read_csv('./data/DOT_03-28-2022 04-15-57-36_timeSeries.csv')
+
+    years = [x for x in df_imf_dot_raw.columns if bool(re.match(r'^\d{4}$', x))]
+
+    # source = 'China, P.R.: Mainland'
+    # destination = 'United States'
+
+    df_imf_dot = (
+        df_imf_dot_raw
+        .query("Attribute == 'Value'")
+        # .query(f"`Country Name` == '{source}' and `Counterpart Country Name` == '{destination}'")
+        .query(
+            """`Indicator Name` in [
+                'Goods, Value of Exports, Free on board (FOB), US Dollars',
+                'Goods, Value of Imports, Cost, Insurance, Freight (CIF), US Dollars',
+                'Goods, Value of Trade Balance, US Dollars'
+            ]""".replace('\n', ''))
+        .loc[:, ['Country Name', 'Counterpart Country Name', 'Indicator Name'] + years]
+        .dropna(how='all', subset=years))
+    df_imf_dot['Indicator Name'].replace({
+        'Goods, Value of Exports, Free on board (FOB), US Dollars': 'Export',
+        'Goods, Value of Imports, Cost, Insurance, Freight (CIF), US Dollars': 'Import',
+        'Goods, Value of Trade Balance, US Dollars': 'Trade Balance'
+    }, inplace=True)
+    for y in years:
+        df_imf_dot[y] = df_imf_dot[y].astype(float)
+    return df_imf_dot
+
+# %%
+
+
+try:
+    df_imf_dot = pd.read_parquet('./data/imf_dot.parq')
+except FileNotFoundError:
+    df_imf_dot = preprecess_imf_dot()
+    df_imf_dot.to_parquet('./data/imf_dot.parq')

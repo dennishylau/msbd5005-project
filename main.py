0ff8dff121f46795eb58b9c4bdea1a9e7cc70b2d
Dennis Lau <dennishylau@gmail.com>
2022-04-15 16:12:42 +0800

    feat: use nav bar

--- main.py
+++ main.py
@@ -1,51 +1,31 @@
 # %%
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-
-# %%
-# page setup
+# %% page setup & layout
 st.set_page_config(layout="wide")
 
-# load data and cache them
-# only init cache after calling set_page_config()
-if True:
-    from cache import dfc_wb_code, dfc_wb_trade, dfc_imf_dot
+title_col, menu_col = st.columns([1, 1])
+with title_col:
+    '# MSBD5005 Project'
+with menu_col:
+    # workaround for vertical alignment
+    ''
+    ''
+    menu = option_menu(
+        None, ['Home', 'World Bank', 'References'],
+        icons=['house', 'globe', 'list-ul'],
+        default_index=0, orientation="horizontal")
+'---'
 
-
-# %% Sidebar
-
-with st.sidebar:
-    '# Filters'
-
-    economies = st.multiselect(
-        'Economies', dfc_wb_code.name.sort_values(),
-        key='economies')
-    if len(economies) == 0:
-        economies = ['World']
-
-    year_start, year_end = st.slider(
-        'Select year',
-        1960, 2020, (1960, 2020)
-    )
-
-# %% Body
-
-"""
-# MSBD5005 Project
-"""
-
-left_column, right_column = st.columns(2)
-
-with left_column:
-    '## Trade (% GDP)'
-    st.line_chart(dfc_wb_trade[economies].loc[year_start: year_end])
-with right_column:
-    st.dataframe(dfc_imf_dot)
-
-# %% Reference
-'''
-## Reference
-- World Bank Trade (% of GDP): https://data.worldbank.org/indicator/NE.TRD.GNFS.ZS
-- WBGAPI for downloading WorldBank data: https://nbviewer.org/github/tgherzog/wbgapi/blob/master/examples/wbgapi-cookbook.ipynb
-- Internation Monetary Fund Direction of Trade Statistics: https://data.imf.org/?sk=9d6028d4-f14a-464c-a2f2-59b2cd424b85&sId=1409151240976
-'''
+if menu == 'Home':
+    # call import here so cache only
+    # init after calling set_page_config()
+    from pages.home import render_home
+    render_home()
+elif menu == 'World Bank':
+    from pages.world_bank import render_wb
+    render_wb()
+elif menu == 'References':
+    from pages import render_references
+    render_references()
---
+++ pages/references.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def render_references():
+    st.write('''
+    ## Reference
+    - World Bank Trade (% of GDP): https://data.worldbank.org/indicator/NE.TRD.GNFS.ZS
+    - WBGAPI for downloading WorldBank data: https://nbviewer.org/github/tgherzog/wbgapi/blob/master/examples/wbgapi-cookbook.ipynb
+    - Internation Monetary Fund Direction of Trade Statistics: https://data.imf.org/?sk=9d6028d4-f14a-464c-a2f2-59b2cd424b85&sId=1409151240976
+    ''')

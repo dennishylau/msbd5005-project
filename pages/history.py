b1bcf6b494fb06dc9791df268d42bccffe8273be
thomas-tf-hk01 <thomaswong@hk01.com>
2022-04-29 18:37:18 +0800

    add timeline and navigation through buttons and tabs.

--- pages/history.py
+++ pages/history.py
@@ -1,31 +1,31 @@
-import altair as alt
 import streamlit as st
-import pandas as pd
+from streamlit_option_menu import option_menu
 from cache import dfc_china_pyramid, dfc_china_data, dfc_china_pop
 from figure.pyramid import get_pyramid
+from streamlit.scriptrunner.script_request_queue import RerunData
+from streamlit.scriptrunner.script_runner import RerunException
 
 
 def get_gdp_series(data, min_year, max_year):
     data = data[
         (data.index >= min_year) &
         (data.index <= max_year)
-    ]
+        ]
     return st.line_chart(data['GDP Per Capita (US $)'])
 
 
 def get_pop_growth_series(data, min_year, max_year):
     pct_change = data.pct_change()
     pct_change = pct_change[
-        (pct_change.index >= min_year) &
-        (pct_change.index <= max_year)
-    ] * 100
+                     (pct_change.index >= min_year) &
+                     (pct_change.index <= max_year)
+                     ] * 100
     pct_change.name = r"population % change"
 
     return st.line_chart(pct_change)
 
 
 def render_1960_1980():
-
     MIN_YEAR = 1960
     MAX_YEAR = 1980
 
@@ -68,23 +68,57 @@ def render_1960_1980():
     st.line_chart(get_pop_growth_series(dfc_china_pop, MIN_YEAR, MAX_YEAR))
 
 
+MENU_NAMES = ['1960 - 1980', '1980 - 2000', '2000 - 2010', '2010 - 2020', '2020 - ?']
+
+
+def next_button_callback(menu_name):
+    current_index = MENU_NAMES.index(menu_name)
+    return MENU_NAMES[current_index + 1]
+
+
+def generate_buttons(current_index):
+    _, back_col, next_col = st.columns([10, 1, 11])
+
+    if current_index > 0:
+        with back_col:
+            if st.button('Back'):
+                st.session_state['timeline_page'] = MENU_NAMES[current_index - 1]
+                raise RerunException(RerunData())
+
+    if current_index < len(MENU_NAMES) - 1:
+        with next_col:
+            if st.button('Next'):
+                st.session_state['timeline_page'] = MENU_NAMES[current_index + 1]
+                raise RerunException(RerunData())
+
+    if current_index == len(MENU_NAMES) - 1:
+        with next_col:
+            if st.button('Explore dashboard'):
+                del st.session_state['timeline_page']
+                return True
+
+
 def render_history():
+    if 'timeline_page' not in st.session_state:
+        st.session_state['timeline_page'] = MENU_NAMES[0]
 
-    with st.container():
-        st.title("China's economics history 1960-2020")
-        col1, col2, col3, col4, col5 = st.columns(5)
+    menu = option_menu(
+        None, MENU_NAMES,
+        icons=['calendar', 'calendar', 'calendar', 'calendar', 'calendar'],
+        default_index=MENU_NAMES.index(st.session_state['timeline_page']), orientation="horizontal")
 
-        with st.container():
-            with col1:
-                st.button('1960-1980')
-            with col2:
-                st.button('1980-2000')
-            with col3:
-                st.button('2000-2010')
-            with col4:
-                st.button('2010-2020')
-            with col5:
-                st.button('2020-?')
+    st.write(st.session_state)
 
-        with st.container():
-            render_1960_1980()
+    if menu == MENU_NAMES[0]:
+        render_1960_1980()
+        generate_buttons(0)
+
+    elif menu == MENU_NAMES[1]:
+        generate_buttons(1)
+    elif menu == MENU_NAMES[2]:
+        generate_buttons(2)
+    elif menu == MENU_NAMES[3]:
+        generate_buttons(3)
+    elif menu == MENU_NAMES[4]:
+        return generate_buttons(4)
+    return False

7a0518b51584e37f3ad88ee4f29026aada81bf3a
pseudorm <chester.ph.cheng@gmail.com>
2022-05-01 16:55:40 +0800

    rearrange 1960 horizontally

--- pages/history.py
+++ pages/history.py
@@ -1,74 +1,129 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-from cache import dfc_china_pyramid, dfc_china_data, dfc_china_pop
+import altair as alt
+from cache import dfc_china_pyramid, dfc_worldbank_gdp
 from figure.pyramid import get_pyramid
+from paragraphs import econ_status_1960, econ_summary_1960, demographic_1960
 from streamlit.scriptrunner.script_request_queue import RerunData
 from streamlit.scriptrunner.script_runner import RerunException
+from streamlit_option_menu import option_menu
+
+
+def percentage_difference(x, y):
+    return (y - x) / x
+
+
+def process_gdp(data):
+    gdp = data[data['Country Code'].isin(
+        ('CHN', 'JPN', "IND",))]
+
+    gdp = gdp.melt(
+        id_vars='Country Name',
+        value_vars=[c for c in data.columns if c.isnumeric()],
+        var_name='year',
+        value_name="GDP (US$)"
+    )
+    gdp.loc[gdp.index, 'year'] = gdp.loc[gdp.index, 'year'].astype(int)
+    return gdp
 
 
 def get_gdp_series(data, min_year, max_year):
-    data = data[
-        (data.index >= min_year) &
-        (data.index <= max_year)
-        ]
-    return st.line_chart(data['GDP Per Capita (US $)'])
+    gdp = process_gdp(data)
+    gdp = gdp[
+        (gdp['year'] >= min_year) &
+        (gdp['year'] <= max_year)
+    ]
+
+    lines = (
+        alt.Chart(gdp)
+        .mark_line()
+        .encode(
+            x='year',
+            y=alt.Y('GDP (US$):Q', axis=alt.Axis(tickCount=10, format=".1e")),
+            color='Country Name:N',
+            strokeDash='Country Name:N'
+        )
+    )
+    # annotation_layer = (
+    #     alt.Chart(gdp)
+    #     .mark_text(size=15, text="", dx=0, dy=0, align="center")
+    #     .encode(
+    #         x="year",
+    #         y="GDP (US$):Q",
+    #         tooltip=["Country Name", "GDP (US$)"],
+    #     )
+    #     .interactive()
+    # )
+    return lines.interactive()
 
 
-def get_pop_growth_series(data, min_year, max_year):
-    pct_change = data.pct_change()
-    pct_change = pct_change[
-                     (pct_change.index >= min_year) &
-                     (pct_change.index <= max_year)
-                     ] * 100
-    pct_change.name = r"population % change"
+def get_gdp_diff(data, start_year, end_year):
+    gdp = process_gdp(data)
+    gdp = gdp[
+        (gdp['year'] == start_year) |
+        (gdp['year'] == end_year)
+    ]
+    gdp = gdp.set_index("year")
+    gdp_diff = (
+        gdp
+        .groupby("Country Name")
+        .apply(
+            lambda x:
+            percentage_difference(
+                x.loc[start_year, "GDP (US$)"],
+                x.loc[end_year, "GDP (US$)"]
+            ) * 100
+        )
+        .reset_index()
+    )
+    gdp_diff = gdp_diff.rename({0: r"GDP % change"}, axis=1)
 
-    return st.line_chart(pct_change)
+    bar = (
+        alt.Chart(gdp_diff)
+        .mark_bar(size=30)
+        .encode(
+            x=alt.X(r"GDP % change:Q",  scale=alt.Scale(domain=(0, 2500))),
+            y='Country Name'
+        )
+        .properties(width=500, height=250)
+    )
+    return bar
 
 
 def render_1960_1980():
     MIN_YEAR = 1960
     MAX_YEAR = 1980
 
-    st.header("1960 - 1980")
-    col1, col2 = st.columns(2)
+    _, col1, _, _ = st.columns([1, 5, 5, 1])
     with col1:
-        st.write(
-            """
-                In the years from 1960 to 1975, China was a very poor country. 
-                The average GDP was aruound X dollars per capita, 
-                that's just a little over Y percent of the US during
-                the same time period.
-                """
-        )
-    with col2:
-        get_gdp_series(dfc_china_data, MIN_YEAR, MAX_YEAR)
-    col1, col2 = st.columns(2)
+        st.header("State of the Chinese economy")
+    _, col1, col2, _ = st.columns([1, 5, 5, 1])
     with col1:
-        st.write(
-            r"""
-                At the beginning of the 60's, much of China's population 
-                is skewed towards the young side, with over X% of the 
-                being in between 0 - 15 years old. We can see a steady 
-                growth in the workforce as the years go by. By the 1980's,
-                China's workforce has grown to Y.
-
-                China's large and young population can provide cost 
-                competitive labour, which benefits its 
-                efforts to open up its economy and in the late 70's.
-
-                Interestingly, the Chinese Communist Party implemented the
-                infamous "One Child Policy" in the year 1980. We will 
-                see the policy's effects on the population and China's 
-                economic future in later chapters.
-            """
-        )
+        st.write(econ_status_1960)
     with col2:
+        gdp_chart = get_gdp_series(
+            dfc_worldbank_gdp, MIN_YEAR, MAX_YEAR)
+        st.altair_chart(gdp_chart, use_container_width=True)
+
+    _, col1, col2, _ = st.columns([1, 5, 5, 1])
+    with col1:
+        st.write(econ_summary_1960)
+    with col2:
+        st.altair_chart(get_gdp_diff(dfc_worldbank_gdp, MIN_YEAR,
+                        MAX_YEAR),  use_container_width=True)
+    ###
+    _, _, col2, _ = st.columns([1, 5, 5, 1])
+    with col2:
+        st.header("The human capital")
+    _, col1, col2, _ = st.columns([1, 5, 5, 1])
+    with col2:
+        st.write(demographic_1960)
+    with col1:
         pyramid = get_pyramid(dfc_china_pyramid, MIN_YEAR, MAX_YEAR)
         st.altair_chart(pyramid, use_container_width=True)
-    st.line_chart(get_pop_growth_series(dfc_china_pop, MIN_YEAR, MAX_YEAR))
 
 
-MENU_NAMES = ['1960 - 1980', '1980 - 2000', '2000 - 2010', '2010 - 2020', '2020 - ?']
+MENU_NAMES = ['1960 - 1980', '1980 - 2000',
+              '2000 - 2010', '2010 - 2020', '2020 - ?']
 
 
 def next_button_callback(menu_name):

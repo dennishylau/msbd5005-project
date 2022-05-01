e50eb9745c838de7f5d86cc32f0dfd6bf07698cc
pseudorm <chester.ph.cheng@gmail.com>
2022-05-01 16:52:39 +0800

    modulize write ups

---
+++ paragraphs/__init__.py
@@ -0,0 +1,42 @@
+econ_status_1960 = """
+    Before the economic reforms in 1978, China maintained Soviet style,
+    centrally planned economy. A large share of the country’s industrial
+    output was directed and controlled by state owned enterprises, which
+    are often bureaucractic and inefficient at distributing resources.
+
+    China also experienced various types of events that saw its growth hindered.
+    Hover the graph on the right to see more. 
+
+    As a result, China's economic development during the 1960s to 1970s
+    was miniscule.
+
+    Comparing the annual GDP of China to countries like Japan and India, we
+    can get a glimpse of its growth relative to other countries at the
+    same time period. All three countries shared a similar GDP at the start,
+    Japan's GDP growth started to diverge around 1965, while China's growth
+    maintained more or less the same as India's.
+"""
+
+econ_summary_1960 = """
+    We can summarize the vicennial of economic development by taking GDP 
+    percent difference between 1960 and 1980. It's obvious that China's
+    economic development was lackluster in this time period. However, all 
+    is about to change in the following years.
+"""
+
+demographic_1960 = r"""
+    At the beginning of the 60's, much of China's population 
+    is skewed towards the young side, with over X% of the 
+    being in between 0 - 15 years old. We can see a steady 
+    growth in the workforce as the years go by. By the 1980's,
+    China's workforce has grown to Y.
+
+    China's large and young population can provide cost 
+    competitive labour, which benefits its 
+    efforts to open up its economy and in the late 70's.
+
+    Interestingly, the Chinese Communist Party implemented the
+    infamous "One Child Policy" in the year 1980. We will 
+    see the policy's effects on the population and China's 
+    economic future in later chapters.
+"""

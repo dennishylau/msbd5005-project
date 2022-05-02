183591adace568e8030f1f7b759edaa0fe0b1110
pseudorm <chester.ph.cheng@gmail.com>
2022-05-02 23:41:23 +0800

    mod 1960 layout

--- pages/history.py
+++ pages/history.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import altair as alt
+import pandas as pd
 from cache import dfc_china_pyramid, dfc_worldbank_gdp
 from figure.pyramid import get_pyramid
-from paragraphs import econ_status_1960, econ_summary_1960, demographic_1960
+from paragraphs import econ_status_1960_1, econ_status_1960_2, demographic_1960
 from streamlit.scriptrunner.script_request_queue import RerunData
 from streamlit.scriptrunner.script_runner import RerunException
 from streamlit_option_menu import option_menu
@@ -12,9 +13,14 @@ def percentage_difference(x, y):
     return (y - x) / x
 
 
-def process_gdp(data):
-    gdp = data[data['Country Code'].isin(
-        ('CHN', 'JPN', "IND",))]
+def process_gdp(data, countries=('CHN', 'JPN', "IND",)):
+    gdp = data[data['Country Code'].isin(countries)]
+    # events = pd.DataFrame(
+    #     [(year, "Great Leap Forward") for year in range(1958, 1962)] +
+    #     [(year, "Cultural Revolution") for year in range(1965, 1976)] +
+    #     [(1976, "TangShang Earthquake"), ],
+    #     columns=['year', 'events']
+    # )
 
     gdp = gdp.melt(
         id_vars='Country Name',
@@ -23,15 +29,32 @@ def process_gdp(data):
         value_name="GDP (US$)"
     )
     gdp.loc[gdp.index, 'year'] = gdp.loc[gdp.index, 'year'].astype(int)
+    # gdp = gdp.merge(events, on='year', how='left')
+
     return gdp
 
 
-def get_gdp_series(data, min_year, max_year):
-    gdp = process_gdp(data)
+def get_gdp_series(
+    data,
+    min_year,
+    max_year,
+    countries=('CHN', 'JPN', "IND",),
+    focus_years=(1960, 1969)
+):
+    focus_start, focus_end = focus_years
+    gdp = process_gdp(data, countries)
     gdp = gdp[
         (gdp['year'] >= min_year) &
         (gdp['year'] <= max_year)
     ]
+
+    gdp_growth = (
+        gdp
+        .set_index('year')['GDP (US$)']
+        .pct_change()
+        .reset_index()
+        .rename({'GDP (US$)': 'Annual GDP growth %'}, axis=1)
+    )
 
     lines = (
         alt.Chart(gdp)
@@ -39,87 +62,97 @@ def get_gdp_series(data, min_year, max_year):
         .encode(
             x='year',
             y=alt.Y('GDP (US$):Q', axis=alt.Axis(tickCount=10, format=".1e")),
-            color='Country Name:N',
-            strokeDash='Country Name:N'
         )
     )
-    # annotation_layer = (
-    #     alt.Chart(gdp)
-    #     .mark_text(size=15, text="", dx=0, dy=0, align="center")
-    #     .encode(
-    #         x="year",
-    #         y="GDP (US$):Q",
-    #         tooltip=["Country Name", "GDP (US$)"],
-    #     )
-    #     .interactive()
-    # )
-    return lines.interactive()
 
-
-def get_gdp_diff(data, start_year, end_year):
-    gdp = process_gdp(data)
-    gdp = gdp[
-        (gdp['year'] == start_year) |
-        (gdp['year'] == end_year)
-    ]
-    gdp = gdp.set_index("year")
-    gdp_diff = (
-        gdp
-        .groupby("Country Name")
-        .apply(
-            lambda x:
-            percentage_difference(
-                x.loc[start_year, "GDP (US$)"],
-                x.loc[end_year, "GDP (US$)"]
-            ) * 100
+    lines_colored = (
+        alt.Chart(gdp)
+        .mark_line()
+        .encode(
+            x='year',
+            y=alt.Y('GDP (US$):Q', axis=alt.Axis(tickCount=10, format=".1e")),
+            color=alt.value('orange')
         )
-        .reset_index()
+        .transform_filter(
+            (alt.datum.year >= focus_start) & (alt.datum.year <= focus_end)
+        )
     )
-    gdp_diff = gdp_diff.rename({0: r"GDP % change"}, axis=1)
 
     bar = (
-        alt.Chart(gdp_diff)
-        .mark_bar(size=30)
+        alt.Chart(gdp_growth)
+        .mark_bar()
         .encode(
-            x=alt.X(r"GDP % change:Q",  scale=alt.Scale(domain=(0, 2500))),
-            y='Country Name'
+            x='year',
+            y=alt.Y('Annual GDP growth %:Q'),
         )
-        .properties(width=500, height=250)
     )
-    return bar
+    bar_colored = (
+        alt.Chart(gdp_growth)
+        .mark_bar()
+        .encode(
+            x='year',
+            y=alt.Y('Annual GDP growth %:Q'),
+            color=alt.value("orange")
+        )
+        .transform_filter(
+            (alt.datum.year >= focus_start) & (alt.datum.year <= focus_end)
+        )
+    )
+
+    return alt.hconcat(
+        lines + lines_colored,
+        bar + bar_colored
+    )
 
 
 def render_1960_1980():
     MIN_YEAR = 1960
     MAX_YEAR = 1980
 
-    _, col1, _, _ = st.columns([1, 5, 5, 1])
-    with col1:
-        st.header("State of the Chinese economy")
-    _, col1, col2, _ = st.columns([1, 5, 5, 1])
-    with col1:
-        st.write(econ_status_1960)
-    with col2:
-        gdp_chart = get_gdp_series(
-            dfc_worldbank_gdp, MIN_YEAR, MAX_YEAR)
-        st.altair_chart(gdp_chart, use_container_width=True)
+    with st.container():
+        _, _, col1, _, _ = st.columns([1, 1, 6, 1, 1])
+        with col1:
+            st.header("State of the Chinese economy")
 
-    _, col1, col2, _ = st.columns([1, 5, 5, 1])
-    with col1:
-        st.write(econ_summary_1960)
-    with col2:
-        st.altair_chart(get_gdp_diff(dfc_worldbank_gdp, MIN_YEAR,
-                        MAX_YEAR),  use_container_width=True)
-    ###
-    _, _, col2, _ = st.columns([1, 5, 5, 1])
-    with col2:
-        st.header("The human capital")
-    _, col1, col2, _ = st.columns([1, 5, 5, 1])
-    with col2:
-        st.write(demographic_1960)
-    with col1:
-        pyramid = get_pyramid(dfc_china_pyramid, MIN_YEAR, MAX_YEAR)
-        st.altair_chart(pyramid, use_container_width=True)
+        _, _, col, _, _ = st.columns([1, 1, 6, 1, 1])
+        with col:
+            st.markdown(econ_status_1960_1, unsafe_allow_html=True)
+            event_selection = st.selectbox('', [
+                'Great famine: 1958-1961',
+                'Cultural revolution: 1965-1975',
+                'Tangshang earthquake: 1976'
+            ])
+
+            if event_selection == 'Great famine: 1958-1961':
+                event_start, event_end = 1958, 1961
+            elif event_selection == 'Cultural revolution: 1965-1975':
+                event_start, event_end = 1965, 1975
+            else:
+                event_start, event_end = 1976, 1977
+
+            gdp_chart = get_gdp_series(
+                dfc_worldbank_gdp,
+                MIN_YEAR,
+                MAX_YEAR,
+                ["CHN"],
+                (event_start, event_end)
+            )
+
+            st.altair_chart(gdp_chart,)
+            st.markdown(
+                econ_status_1960_2
+            )
+
+    with st.container():
+        _, _, col1, _, _ = st.columns([1, 1, 6, 1, 1])
+        with col1:
+            st.header("The human capital")
+        _, _, col1, col2, _, _ = st.columns([1, 1, 3, 3, 1, 1])
+        with col1:
+            st.write(demographic_1960)
+        with col2:
+            pyramid = get_pyramid(dfc_china_pyramid, MIN_YEAR, MAX_YEAR)
+            st.altair_chart(pyramid, use_container_width=True)
 
 
 MENU_NAMES = ['1960 - 1980', '1980 - 2000',

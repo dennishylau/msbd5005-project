5b91188ce7ae5ae5714ead7bd4f43339499e61a0
pseudorm <chester.ph.cheng@gmail.com>
2022-05-07 00:13:10 +0800

    refactoring render_1960_180

--- pages/history.py
+++ pages/history.py
@@ -16,56 +16,7 @@ from streamlit.scriptrunner.script_request_queue import RerunData
 from streamlit.scriptrunner.script_runner import RerunException
 from streamlit_option_menu import option_menu
 from pages.periods.render_2001_2010 import render_2001_2010
-
-
-def render_1960_1980():
-    MIN_YEAR = 1960
-    MAX_YEAR = 1980
-
-    with st.container():
-        _, _, col1, _, _ = st.columns([1, 1, 6, 1, 1])
-        with col1:
-            st.header("State of the Chinese economy")
-
-        _, _, col, _, _ = st.columns([1, 1, 6, 1, 1])
-        with col:
-            st.markdown(econ_status_1960_1, unsafe_allow_html=True)
-            event_selection = st.selectbox('', [
-                'Great famine: 1958-1961',
-                'Cultural revolution: 1965-1975',
-                'Tangshang earthquake: 1976'
-            ])
-
-            if event_selection == 'Great famine: 1958-1961':
-                event_start, event_end = 1958, 1961
-            elif event_selection == 'Cultural revolution: 1965-1975':
-                event_start, event_end = 1965, 1975
-            else:
-                event_start, event_end = 1976, 1977
-
-            gdp_chart = get_gdp_charts(
-                dfc_worldbank_gdp,
-                MIN_YEAR,
-                MAX_YEAR,
-                ["CHN"],
-                (event_start, event_end)
-            )
-
-            st.altair_chart(gdp_chart,)
-            st.markdown(
-                econ_status_1960_2
-            )
-
-    with st.container():
-        _, _, col1, _, _ = st.columns([1, 1, 6, 1, 1])
-        with col1:
-            st.header("The human capital")
-        _, _, col1, col2, _, _ = st.columns([1, 1, 3, 3, 1, 1])
-        with col1:
-            st.write(demographic_1960)
-        with col2:
-            pyramid = get_pyramid(dfc_china_pyramid, MIN_YEAR, MAX_YEAR)
-            st.altair_chart(pyramid, use_container_width=True)
+from pages.periods.render_1960_1980 import render_1960_1980
 
 
 MENU_NAMES = ['1960 - 1980', '1981 - 2000',
@@ -109,7 +60,7 @@ def render_history():
         default_index=MENU_NAMES.index(st.session_state['timeline_page']), orientation="horizontal")
 
     if menu == MENU_NAMES[0]:
-        render_1960_1980()
+        render_1960_1980(dfc_worldbank_gdp, dfc_china_pyramid)
         generate_buttons(0)
 
     elif menu == MENU_NAMES[1]:

ba906cfb77b6ac9c7923456b16056c795d871c5d
pseudorm <chester.ph.cheng@gmail.com>
2022-05-07 11:00:17 +0800

    rearrange render 2001

--- pages/periods/render_2001_2010.py
+++ pages/periods/render_2001_2010.py
@@ -32,7 +32,8 @@ def render_2001_2010(dfc_china_data, dfc_imf_dot):
                 t=30,
                 pad=0,
                 autoexpand=False))
-        fig.add_vline(x=2001, line_dash="dot", annotation_text="Joined WTO", annotation_position='top left')
+        fig.add_vline(x=2001, line_dash="dot",
+                      annotation_text="Joined WTO", annotation_position='top left')
         fig.add_vline(x=2002, line_dash="dot", annotation_position='top right',
                       annotation_text='ASEAN–China Free Trade Area (ACFTA) established')
         fig.add_vline(x=2008, line_dash="dot", annotation_position='top right',
@@ -41,9 +42,9 @@ def render_2001_2010(dfc_china_data, dfc_imf_dot):
                       annotation_position='bottom right', annotation_text='Global Financial Crisis',
                       annotation_font={'color': '#ff3b3b', 'size': 15})
 
-    st.header("2001 - 2010")
-    desc_col, fig_col = st.columns([1, 1])
+    _, _, desc_col, _, _ = st.columns([1, 1, 6, 1, 1])
     with desc_col:
+        st.header("2001 - 2010")
         st.write('''
         If we look at the Trade Balance of China, we can see that China has not been opened to trades in the 90s.
         However, China became a member of the World Trade Organization (WTO) on 11 December 2001. China's accession
@@ -58,15 +59,12 @@ def render_2001_2010(dfc_china_data, dfc_imf_dot):
         On 8 August 2008, the Beijing Olympics commenced. China's trade balance reached the peak. However, the impact of
         the Global Financial Crisis that took place between 2007 and 2008 had a huge effect on China's GDP Growth.
         ''')
-
-    with fig_col:
         for fig in figs:
             st.plotly_chart(fig, use_container_width=True)
 
-    st.header("WTO & ACFTA impacts")
-    desc_col, fig_col = st.columns([1, 1])
-
+    _, _, desc_col, _, _ = st.columns([1, 1, 6, 1, 1])
     with desc_col:
+        st.header("WTO & ACFTA impacts")
         st.write('''       
         On the right, we have a map showing China's top 10 best trade partners and worst 10 trade partners 
         in terms of trade balance. You can slide through the slide bar from 2002 to 2010. Green indicates profit and
@@ -78,13 +76,13 @@ def render_2001_2010(dfc_china_data, dfc_imf_dot):
         ACFTA framework.
         ''')
 
-    with fig_col:
         chosen_country = 'China'
         min_year = 2002
         max_year = 2010
         chosen_year = st.slider('Year', min_year, max_year, 2002)
 
-        data = update_data(dfc_imf_dot, chosen_country, chosen_year, chosen_top_n=10, chosen_bottom_n=10)
+        data = update_data(dfc_imf_dot, chosen_country,
+                           chosen_year, chosen_top_n=10, chosen_bottom_n=10)
         trade_balance_map = plot_trade_balance_map(data, chosen_country, chosen_year, chosen_top_n=10,
                                                    chosen_bottom_n=10)
         st.plotly_chart(trade_balance_map, use_container_width=True)

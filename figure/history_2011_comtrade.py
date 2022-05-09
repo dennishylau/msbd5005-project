253d1382666b24b0f4ae8a718c6e03ccd6bc5dda
Dennis Lau <dennishylau@gmail.com>
2022-05-10 00:02:43 +0800

    refactor: change fig_goods color, remove pickle cache

--- figure/history_2011_comtrade.py
+++ figure/history_2011_comtrade.py
@@ -3,25 +3,29 @@ import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit.components.v1 as components
-from cache import dfc_comtrade, pickle_cache
+from cache import dfc_comtrade
+
+COLORS_TARIFF = {
+    '(?)': '#2f3452',  # mix category
+    'false': 'grey',  # no additional tariff
+    'true': 'red'  # has tariff
+}
 
 
-@pickle_cache('fig_goods')
-def get_fig_goods():
+def get_fig_goods(height=600):
     fig_goods = (
         px.treemap(
             dfc_comtrade,
             path=['Trade Flow', 'Commodity_short',
                   'Commodity_agg_4_short', 'Commodity_agg_6_short'],
             values='Trade Value (US$)', maxdepth=-1, color='tariff',
-            color_discrete_map={'(?)': 'grey', 'false': 'green',
-                                'true': 'red'},
+            color_discrete_map=COLORS_TARIFF,
             custom_data=dfc_comtrade
             [['Commodity_long', 'Commodity_agg_4_long',
               'Commodity_agg_6_long']],)
         .update_layout(
             title='Commodity Categories, China Export to United States',
-            height=600,
+            height=height,
             margin=dict(t=50, l=25, r=25, b=25))
         .update_traces(
             root_color='#383948', textfont=dict(color="white"),
@@ -46,7 +50,7 @@ def get_fig_goods():
             y=[1, 3],
             textposition='auto',
             name='Categories with Additional Traiff',
-            marker_color='red')
+            marker_color=COLORS_TARIFF['true'])
     )
     fig_goods.add_trace(
         go.Bar(
@@ -54,7 +58,7 @@ def get_fig_goods():
             y=[1, 3],
             textposition='auto',
             name='Categories without Additional Traiff',
-            marker_color='green')
+            marker_color=COLORS_TARIFF['false'])
     )
     fig_goods.add_trace(
         go.Bar(
@@ -62,7 +66,7 @@ def get_fig_goods():
             y=[1, 3],
             textposition='auto',
             name='Mixed Categories',
-            marker_color='grey')
+            marker_color=COLORS_TARIFF['(?)'])
     )
     fig_goods.update_xaxes(visible=False)
     fig_goods.update_yaxes(visible=False)
@@ -70,14 +74,14 @@ def get_fig_goods():
     return fig_goods
 
 
-def write_fig_goods():
+def write_fig_goods(height=600):
     'Read the cached python obj / html to speed up loading'
     try:
         with open('cached_html/fig_goods.html') as html:
-            # call from html cache to speed up rendering
-            components.html(html.read(), height=600)
+            # get cached html to speed up rendering
+            components.html(html.read(), height=height)
     except FileNotFoundError:
-        fig_goods = get_fig_goods()
+        fig_goods = get_fig_goods(height=height)
         # write to html as cache
         fig_goods.write_html(
             file='cached_html/fig_goods.html',

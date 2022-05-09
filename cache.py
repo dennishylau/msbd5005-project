3b61cfba7850c861945ea25c5d8a02924b16befb
pseudorm <chester.ph.cheng@gmail.com>
2022-05-09 17:31:35 +0800

    quick fix

--- cache.py
+++ cache.py
@@ -13,12 +13,12 @@ from preprocess import (
     df_china_data,
     df_china_pyramid,
     df_china_pop,
-    df_china_gdp_comp
-)
+    df_china_gdp_comp,
     df_china_tariffs,
-    df_comtrade)
+    df_comtrade
+)
 
-COUNTRY_CODE_MAPPING=df_wb_code.set_index('code')['name'].to_dict()
+COUNTRY_CODE_MAPPING = df_wb_code.set_index('code')['name'].to_dict()
 
 
 @ st.experimental_memo
@@ -30,7 +30,7 @@ def pickle_cache(cache_key: str):
     '''
     Pickle python object to increase performance
     '''
-    path=f'pickle/{cache_key}'
+    path = f'pickle/{cache_key}'
 
     def decorator(fn):
         def wrapped(*args, **kwargs):
@@ -39,7 +39,7 @@ def pickle_cache(cache_key: str):
                 with open(path, 'rb') as c:
                     return pickle.load(c)
             # else generate obj
-            obj=fn(*args, **kwargs)
+            obj = fn(*args, **kwargs)
             with open(path, 'wb') as c:
                 pickle.dump(obj, c)
             return obj
@@ -57,40 +57,40 @@ def get_valid_countries(df) -> list[str]:
     :param df: pd.DataFrame. Cached DataFrame preprocessed for trade balance map.
     :return valid_countries: list[str]. List of valid countries names in str.
     """
-    valid_countries=df['Country Name'].unique().tolist()
+    valid_countries = df['Country Name'].unique().tolist()
     return valid_countries
 
 
 def align_country_name(df: pd.DataFrame, country_name_col: str, code_col: str):
-    df[country_name_col]=df[code_col].map(COUNTRY_CODE_MAPPING)
-    df.dropna(subset = [country_name_col], inplace = True)
+    df[country_name_col] = df[code_col].map(COUNTRY_CODE_MAPPING)
+    df.dropna(subset=[country_name_col], inplace=True)
     return df
 
 
-df_imf_dot=align_country_name(
+df_imf_dot = align_country_name(
     df_imf_dot, 'Country Name', 'Country Code ISO3')
-df_imf_dot=align_country_name(
+df_imf_dot = align_country_name(
     df_imf_dot, 'Counterpart Country Name', 'Counterpart Country Code ISO3')
-df_worldbank_gdp=align_country_name(
+df_worldbank_gdp = align_country_name(
     df_worldbank_gdp, 'Country Name', 'Country Code')
-df_population_15_64_percent=align_country_name(
+df_population_15_64_percent = align_country_name(
     df_population_15_64_percent, 'Country Name', 'Country Code')
 
-valid_countries=get_valid_countries(df_imf_dot)
+valid_countries = get_valid_countries(df_imf_dot)
 
 # cache
-dfc_wb_code=__cache(df_wb_code)
-dfc_wb_trade=__cache(df_wb_trade)
-dfc_imf_dot=__cache(
+dfc_wb_code = __cache(df_wb_code)
+dfc_wb_trade = __cache(df_wb_trade)
+dfc_imf_dot = __cache(
     df_imf_dot
     [(df_imf_dot['Country Name'].isin(valid_countries)) &
      (df_imf_dot['Counterpart Country Name'].isin(valid_countries))])
-dfc_china_data=__cache(df_china_data)
-dfc_worldbank_gdp=__cache(df_worldbank_gdp)
-dfc_worldbank_gdp_per_cap=__cache(df_worldbank_gdp_per_cap)
-dfc_population_15_64_percent=__cache(df_population_15_64_percent)
-dfc_china_pyramid=__cache(df_china_pyramid)
-dfc_china_pop=__cache(df_china_pop)
-dfc_china_gdp_comp=__cache(df_china_gdp_comp)
-dfc_china_tariffs=__cache(df_china_tariffs)
-dfc_comtrade=__cache(df_comtrade)
+dfc_china_data = __cache(df_china_data)
+dfc_worldbank_gdp = __cache(df_worldbank_gdp)
+dfc_worldbank_gdp_per_cap = __cache(df_worldbank_gdp_per_cap)
+dfc_population_15_64_percent = __cache(df_population_15_64_percent)
+dfc_china_pyramid = __cache(df_china_pyramid)
+dfc_china_pop = __cache(df_china_pop)
+dfc_china_gdp_comp = __cache(df_china_gdp_comp)
+dfc_china_tariffs = __cache(df_china_tariffs)
+dfc_comtrade = __cache(df_comtrade)
